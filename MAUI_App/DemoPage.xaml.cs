d9eac073cdb4f3050114f4e45153e150a5a3554d
birbalkr <birbalkr1435@gmail.com>
2024-04-08 11:12:47 +0530

    NavigationPage

---
+++ MAUI_App/DemoPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MAUI_App;
+
+public partial class DemoPage : ContentPage
+{
+	public DemoPage()
+	{
+		InitializeComponent();
+	}
+}
--- MAUI_App/obj/Debug/net8.0-android/MAUI_App.AssemblyInfo.cs
+++ MAUI_App/obj/Debug/net8.0-android/MAUI_App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MAUI_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+bb08ec6af2d8f9dc827d9e2909453a4f2a425342")]
 [assembly: System.Reflection.AssemblyProductAttribute("MAUI_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MAUI_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
